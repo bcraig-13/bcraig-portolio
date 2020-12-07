2d8d7fa56e9773bb52bc7a1c21acc37df9689cbf
Brandon Craig <thegamingraven13@gmail.com>
2020-12-07 09:39:29 -0800

    Made the navbar links align on the right on every page.

--- button.js
+++
@@ -1,6 +0,0 @@
-function button {
-    document.getElementById('Name').value = "";
-    document.getElementById('Email').value = "";
-    document.getElementById('Comment').value = "";
-    alert("Thank you! You're message has been submitted.");
-}
