f0e24fd76b097fce51213446f4e70436184f2558
Brandon Craig <thegamingraven13@gmail.com>
2020-12-07 08:59:21 -0800

    Added javascript button to contatcs.

---
+++ button.js
@@ -0,0 +1,6 @@
+function button {
+    document.getElementById('Name').value = "";
+    document.getElementById('Email').value = "";
+    document.getElementById('Comment').value = "";
+    alert("Thank you! You're message has been submitted.");
+}
